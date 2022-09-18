19e321c741f7e3cf6e34805e6617b14e9a86ae64
MandoPurba <romando.purba@yahoo.com>
2022-09-18 22:26:17 +0700

    Laravel rest api

---
+++ database/migrations/2022_09_18_141617_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->year('year');
+            $table->string('author');
+            $table->text('summary');
+            $table->string('publisher');
+            $table->integer('page_count');
+            $table->integer('read_page');
+            $table->boolean('finished')->default(false);
+            $table->boolean('reading')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
